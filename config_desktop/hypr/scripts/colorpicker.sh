73ed1d8ffd4ed798fee962b06364e0d70a543e77
CarlosGarcia20 <cargarcia085@gmail.com>
2026-05-09 20:11:08 -0700

    Eliminacion de archivos basura para el script de instalacion

--- config_desktop/hypr/scripts/colorpicker.sh
+++
@@ -1,6 +0,0 @@
-color=$(hyprpicker -a | grep '^#')
-if [ -n "$color" ]; then
-    notify-send "🎨 Color copiado" "<span fgcolor='$color'>$color</span>"
-else
-    notify-send "❌ No se seleccionó ningún color"
-fi
